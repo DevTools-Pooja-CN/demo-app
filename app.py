50a8ea25af4179d9834ae11bf6394ac880ca94ee
Charan <charanv@devtools.in>
2025-07-29 17:45:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello Charan, Welcome to Jenkins Pipeline with Python!"
+    return "Hello, Jenkins Pipeline with Python!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3001)
