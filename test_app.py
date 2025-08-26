ad1dd56403a73198e2376dd7f9582e47ee902a23
Pooja-DevTools <poojan@devtools.in>
2025-08-26 11:35:22 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -9,6 +9,7 @@ def client():
 def test_home(client):
     response = client.get("/")
     assert response.status_code == 200
-    assert b"Welcome!" in response.data
+    assert b"Welcome" in response.data
     assert b"Jenkins Python App" in response.data
-    assert b"deployed using a Jenkins pipeline using docker" in response.data
+    assert b"built and deployed" in response.data
+    assert b"Jenkins pipeline with Docker" in response.data or b"Docker & AKS" in response.data
