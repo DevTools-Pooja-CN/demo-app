aa38bd61b6524325e03360b8ef67d166a0c712f5
Pooja-DevTools <poojan@devtools.in>
2025-08-26 11:47:16 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -9,7 +9,7 @@ def client():
 def test_home(client):
     response = client.get("/")
     assert response.status_code == 200
-    assert b"Welcome" in response.data
-    assert b"Jenkins Python App" in response.data
-    assert b"built and deployed" in response.data
-    assert b"Jenkins pipeline with Docker" in response.data or b"Docker & AKS" in response.data
+    assert b"Welcome to DevTools" in response.data or b"Hello DevOps Enthusiast" in response.data
+    assert b"DevTools is a leading consulting" in response.data
+    assert b"DevSecOps" in response.data
+    assert b"Automation" in response.data
